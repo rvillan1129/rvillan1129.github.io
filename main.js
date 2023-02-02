fe88964068fc80674e75cd96f0b74e487c716d0d
rvillan1129 <r.villan1129@gmail.com>
2023-02-01 20:45:29 -0500

    re-did entire page
    added new background
    re-shaped site cards

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-function showMore(){
-    alert("You have clicked the header div!");
+function reveal(divName, partnerName){
+    hiddenState = document.querySelector(divName).hidden;
+
+    if(hiddenState){
+        document.querySelector(divName).hidden = false;
+        
+        document.querySelector(partnerName).style.border = "3px black solid";
+        document.querySelector(partnerName).style.flex = "3";
+    }
+    if(!hiddenState){
+        document.querySelector(divName).hidden = true;
+
+        document.querySelector(partnerName).style.border = "8px black solid";
+        document.querySelector(partnerName).style.flex = "1";
+    }
+    
+}
+
+function visitSite(siteName){
+    window.open(siteName, "_blank");
 }
